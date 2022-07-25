dbb0130c961becc16db0720861eb9322ef9119a0
Lama Radwan <lama.radwan96@gmail.com>
2022-07-25 16:21:47 +0300

    Added the django project and app

---
+++ snacks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+
+from snacks.views import HomePage,AboutPage
+
+urlpatterns = [
+    path('', HomePage.as_view(), name='home'),
+    path('about/',AboutPage.as_view(), name='about')
+]
